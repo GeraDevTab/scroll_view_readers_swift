baebc9b5f82e0e54836befa570eb7bfcbc81ac17
Gerardo Custodio <everythingissofast@gmail.com>
2026-06-30 09:40:11 -0600

    first commit scrol view readers swift

---
+++ ScrollViewReaders/ScrollViewReadersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewReadersApp.swift
+//  ScrollViewReaders
+//
+//  Created by Gerardo J C on 30/06/26.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewReadersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
